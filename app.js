7f61bdf50a9f14c2a28bbc537068bd48813be10a
talemanjan <talemanaptech@gmail.com>
2023-03-20 15:08:11 +0500

    added javscript

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ function add() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
   
+    if (!isNaN(num1) && !isNaN(num2)) {
+      let sum = num1 + num2;
+    }
   }
   
 
