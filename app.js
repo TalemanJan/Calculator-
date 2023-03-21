152e853df25f82609a1db7df0355fdc6194ae1b8
talemanjan <talemanaptech@gmail.com>
2023-03-21 14:36:32 +0500

    added js

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ document.getElementById("result").innerHTML="Please enter valid numbers."
 
  function divide(){
 if(!isNaN(num1) && !isNaN(num2))
-
-let divide=num1/num2
+{
+let divide=num1/num2;
+document.getElementById("result").innerHTML=`The Sum is : ${divide} `
+}
 
  }
