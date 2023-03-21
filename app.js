a2d33fafce2ab69b3c5788485ec68c89c22a6c75
talemanjan <talemanaptech@gmail.com>
2023-03-21 14:03:06 +0500

    added js

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ document.getElementById("result").innerHTML="Please enter valid numbers."
   let num1=parseInt(document.getElementById('num1').value)
   let num2=parseInt(document.getElementById('num2').value)
 
+if(!isNaN(num1) && !isNaN(num2))
+{
+  let multiply=num1*num2
+    document.getElementById("result").innerHTML=`The Sum is : ${multiply} `
+
+}
 
 
 
